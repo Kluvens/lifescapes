169e1b7371c4f68a0ad35d7bfdafdc3089a573e6
Kluvens <z5339252@ad.unsw.edu.au>
2023-08-16 10:55:42 +1000

    added log4j2

--- src/main/java/com/lifescapes/lifescapes/LifescapesApplication.java
+++ src/main/java/com/lifescapes/lifescapes/LifescapesApplication.java
@@ -1,13 +1,24 @@
 package com.lifescapes.lifescapes;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class LifescapesApplication {
+public class LifescapesApplication implements ApplicationRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(LifescapesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LifescapesApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		logger.info("Application Running");
 	}
 
 }
