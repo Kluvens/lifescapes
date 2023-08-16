e18244e333ec99f3f046edc644149e03465f8027
Kluvens <z5339252@ad.unsw.edu.au>
2023-08-16 10:18:55 +1000

    init

---
+++ src/main/java/com/lifescapes/lifescapes/LifescapesApplication.java
@@ -0,0 +1,13 @@
+package com.lifescapes.lifescapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifescapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifescapesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifescapes/lifescapes/LifescapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifescapes.lifescapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifescapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
